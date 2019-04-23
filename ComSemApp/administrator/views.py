7ab8457f782b712ae4defefb66bbec5bb19cbab2
Daniel Richard <daniel.richard@outlook.com>
2019-04-23 16:38:19 -0700

    update

--- ComSemApp/administrator/views.py
+++ ComSemApp/administrator/views.py
@@ -134,7 +134,7 @@ class StudentListView(AdminViewMixin, ListView):
                         break
                     if (fields[0].isalpha() == False or fields[1].isalpha() == False):
                         print(fields[1].isalpha())
-                        print(field[1])
+                        print(fields[1])
                         message = (str(linecount) + " " + fields[0] + " " + fields[1] + "      " + fields[2] + "       " + fields[3] + "        Invalid First or Last Name \n")
                         message_content.append(message)
                         rejectcount += 1
